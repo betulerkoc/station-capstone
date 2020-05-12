d56e01817dfab5ba4e4ddfffc1d44328e2898f1b
Betul Erkoc <betulfatmaerkoc@gmail.com>
2020-05-12 23:46:44 +0300

    tables finished

--- app/src/main/java/com/example/stationproject/MainActivity.java
+++ app/src/main/java/com/example/stationproject/MainActivity.java
@@ -6,39 +6,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.Checkable;
 import android.widget.EditText;
 import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    SQLiteOpenHelper openHelper;
-    SQLiteDatabase db;
-    Button buttonStation;
-    EditText loc, p, t;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        openHelper = new SQLiteHelper(this);
-
-        buttonStation = findViewById(R.id.buttonStation);
-        loc = findViewById(R.id.loc);
-        p = findViewById(R.id.price);
-        t = findViewById(R.id.type);
-
-        buttonStation.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                db = openHelper.getWritableDatabase();
-                String location = loc.getText().toString();
-                String price = p.getText().toString();
-                String type = t.getText().toString();
-                SQLiteHelper.insertStation(db, location, price, type);
-                Toast.makeText(getApplicationContext(), "station", Toast.LENGTH_LONG).show();
-            }
-        });
-
-    }
 }
--- app/src/main/java/com/example/stationproject/SQLiteHelper.java
+++ app/src/main/java/com/example/stationproject/SQLiteHelper.java
@@ -23,8 +23,24 @@ public class SQLiteHelper extends SQLiteOpenHelper {
                 "stationID INTEGER PRIMARY KEY AUTOINCREMENT, " +
                 "Location TEXT COLLATE NOCASE, " +
                 "Type TEXT, " +
-                "Price TEXT);";
+                "Price TEXT, " +
+                "Availability NUMERIC, " +
+                "CharginStatus NUMERIC, " +
+                "Capacity INTEGER);";
     }
+
+    public String createReservation() {
+        return "CREATE TABLE RESERVATION (" +
+                "reservationID INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                "reservationDate DATE, "+
+                "reservationTime TIME, "+
+                "reservationStatus NUMERIC, " +
+                "userID INTEGER, " +
+                "stationID INTEGER, " +
+                "FOREIGN KEY(userID) REFERENCES USER(userID), " +
+                "FOREIGN KEY(stationID) REFERENCES STATION(stationID));";
+    }
+
 
 
     public SQLiteHelper(Context context) {
@@ -35,23 +51,28 @@ public class SQLiteHelper extends SQLiteOpenHelper {
     public void onCreate(SQLiteDatabase db) {
         db.execSQL(createUser());
         db.execSQL(createStation());
+        db.execSQL(createReservation());
 
+        makeReservation(db, "2020-8-12","10:30", true, 5, 8);
+        makeReservation(db, "2020-7-3", "12:00",true, 5, 8);
+        makeReservation(db, "2020-9-7", "15:30",true, 5, 8);
+
+        insertUser(db, "Betul", "Erkoç", "123", "betul@gmail.com");
+        insertUser(db, "Ayse", "Aydın", "123", "ayse@gmail.com");
+        insertUser(db, "Cansu", "Gunay", "123", "cansu@gmail.com");
+
+        insertStation(db, "Kızılelma Caddesi", "5", "DC", true, false, 10);
+        insertStation(db, "Dolmabahçe Caddesi", "5", "DC", false, true, 15);
+        insertStation(db, "Vali Konağı Caddesi", "5", "DC", true, false, 5);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         db.execSQL("DROP TABLE IF EXISTS " + "USER");
         db.execSQL("DROP TABLE IF EXISTS " + "STATION");
+        db.execSQL("DROP TABLE IF EXISTS " + "RESERVATION");
+
         onCreate(db);
-    }
-
-    public static void insertStation(SQLiteDatabase db, String loc, String price, String type) {
-        ContentValues stationValues = new ContentValues();
-        stationValues.put("Location", loc);
-        stationValues.put("Price", price);
-        stationValues.put("Type", type);
-
-        long id = db.insert("STATION", null, stationValues);
     }
 
     public static void insertUser(SQLiteDatabase db, String fname, String sname, String pass, String email) {
@@ -61,8 +82,29 @@ public class SQLiteHelper extends SQLiteOpenHelper {
         userValues.put("Password", pass);
         userValues.put("Email", email);
 
-        long id = db.insert("USER", null, userValues);
+        db.insert("USER", null, userValues);
     }
 
+    public static void insertStation(SQLiteDatabase db, String loc, String price, String type, boolean availability, boolean charging, int capacity) {
+        ContentValues stationValues = new ContentValues();
+        stationValues.put("Location", loc);
+        stationValues.put("Price", price);
+        stationValues.put("Type", type);
+        stationValues.put("Availability", availability);
+        stationValues.put("CharginStatus", charging);
+        stationValues.put("Capacity", capacity);
 
+        db.insert("STATION", null, stationValues);
+    }
+
+    public static void makeReservation (SQLiteDatabase db, String resDate,String resTime, boolean resStatus, int userID, int stationID) {
+        ContentValues stationValues = new ContentValues();
+        stationValues.put("reservationDate", resDate);
+        stationValues.put("reservationTime", resTime);
+        stationValues.put("reservationStatus", resStatus);
+        stationValues.put("userID", userID);
+        stationValues.put("stationID", stationID);
+
+        db.insert("RESERVATION", null, stationValues);
+    }
 }
